f2909fae30338f98e521f3d97a7fa55c3b0a2830
Corey Walo <cjwalo@gmail.com>
2024-07-01 20:10:12 -0700

    Initial Commit

---
+++ SharedStackState/SharedStackStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedStackStateApp.swift
+//  SharedStackState
+//
+//  Created by Corey Walo on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SharedStackStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
